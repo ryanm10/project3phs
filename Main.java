d64a79e7f26dae7aa87262b282c3b328f58930a3
Ryan <ryan.milton@patelhs.org>
2020-11-12 14:07:14 +0000

    Formatted code better

--- Main.java
+++ Main.java
@@ -2,13 +2,14 @@
 //November 12th 2020 
 //Period 3 Gaber
 
+
 import java.util.Scanner; //Scanner to be imported
 class Main {
   public static void main(String[] args) {
     int [] learnerSeats = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }; //These are the seat numbers, as there are 15 people in the class, there are 15 numbers.
-    String[] newNames = new String[15]; //Because there are 15, we put string15
+      String[] newNames = new String[15]; //Because there are 15, we put string15
      Scanner reader = new Scanner(System.in);
-     System.out.println("Welcome! To begin, as you know it's the first day of school. (Yay!) As a way to help, please give 15 names, so it can be easier to assign a student to a seat, as I'm confused. Thank you!"); //This is where they will have to enter in 15 names from here, and then their input will be recieved. It's meant to be a nice little introduction, so they understand what to do and the story behind it. 
+      System.out.println("Welcome! To begin, as you know it's the first day of school. (Yay!) As a way to help, please give 15 names, so it can be easier to assign a student to a seat, as I'm confused. Thank you!"); //This is where they will have to enter in 15 names from here, and then their input will be recieved. It's meant to be a nice little introduction, so they understand what to do and the story behind it. 
       for(int x = 0; x < 15; x++)
      {
          String name = reader.nextLine();
